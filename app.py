81c401fa8a0dcb80df405b55f0030e0e83acf137
Jordan <111466159+JRDN-tech@users.noreply.github.com>
2022-11-29 12:38:58 -0500

    Update app.py
    added about.html in app

--- app.py
+++ app.py
@@ -30,6 +30,9 @@ db = SQL("sqlite:///finance.db")
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
 
+@app.route("/about")
+def about():
+    return render_tempalte("about.html")
 
 @app.after_request
 def after_request(response):
